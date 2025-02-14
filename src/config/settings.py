bf4f407e4349d20df4fcadfd0a9a6ccc22140ec2
duendereus <cespedesfernando967@gmail.com>
2025-02-13 21:07:05 -0600

    One more db try

--- src/config/settings.py
+++ src/config/settings.py
@@ -91,17 +91,25 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "neondb",
-        "USER": "neondb_owner",
-        "PASSWORD": "npg_E8bHfoI4wATC",
-        "HOST": "ep-plain-shadow-a6veo1x1-pooler.us-west-2.aws.neon.tech",
-        "PORT": "5432",
-        "OPTIONS": {
-            "sslmode": "require",
-        },
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=30)
+DATABASE_URL = "postgresql://neondb_owner:npg_E8bHfoI4wATC@ep-plain-shadow-a6veo1x1-pooler.us-west-2.aws.neon.tech/neondb?sslmode=verify-full"
+
+
+if DATABASE_URL is not None:
+    import dj_database_url
+
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=CONN_MAX_AGE,
+            conn_health_checks=True,
+        )
+    }
 
 # Replace the DATABASES section of your settings.py with this
 # DATABASES = {
